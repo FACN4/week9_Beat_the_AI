bfaf7d09a9f2542c444e36b8fbce3138d9dfd41f
afifisuha <afifisuha@campus.technion.ac.il>
2018-08-30 11:43:56 +0300

    #19 replaced with functions

--- src/components/GameBody.js
+++ src/components/GameBody.js
@@ -1,7 +1,7 @@
 import React from "react";
-import GridContainer from "./gameComponents/GridContainer";
+import GridContainer from "./container/GridContainer";
 import { Header } from "./gameComponents/Header";
-import TurnLabel from "./gameComponents/TurnLabel";
+import { TurnLabel } from "./gameComponents/TurnLabel";
 import { aiTurn } from "../logic/aiTurn";
 
 export default class GameBody extends React.Component {
@@ -13,28 +13,34 @@ export default class GameBody extends React.Component {
     let { boardStatus, humanTurn } = this.state;
     const { humanSymbol, gameMode } = this.props;
     this.setState(
-      (prevState) => {
+      prevState => {
         return { humanTurn: !prevState.humanTurn };
       },
       () => {
         if (!this.state.humanTurn) {
           console.log("should execute AITurn");
-          const aiChoice = aiTurn(boardStatus, gameMode,humanSymbol);
-          const aiSymbol = humanSymbol === 'X'? 'O':'X';
-          setTimeout( () => {this.changeBoard(aiChoice, aiSymbol)},800);
-
+          const aiChoice = aiTurn(boardStatus, gameMode, humanSymbol);
+          const aiSymbol = humanSymbol === "X" ? "O" : "X";
+          setTimeout(() => {
+            this.changeBoard(aiChoice, aiSymbol);
+          }, 800);
         }
       }
     );
   };
   changeBoard = (index, symbol) => {
-    this.setState(({ boardStatus }) => {
-      boardStatus[index] = symbol;
-      return { boardStatus: boardStatus };
-    }, () => {
-      const gameOver = this.props.aWin(this.state.boardStatus, symbol);
-      if (!gameOver) {this.changeTurn()}
-    });
+    this.setState(
+      ({ boardStatus }) => {
+        boardStatus[index] = symbol;
+        return { boardStatus: boardStatus };
+      },
+      () => {
+        const gameOver = this.props.aWin(this.state.boardStatus, symbol);
+        if (!gameOver) {
+          this.changeTurn();
+        }
+      }
+    );
   };
 
   render() {
--- src/components/gameComponents/TurnLabel.js
+++ src/components/gameComponents/TurnLabel.js
@@ -1,12 +1,8 @@
 import React from "react";
 import "../../styles/turnLabel";
 
-export default class TurnLabel extends React.Component {
-  render() {
-    const {humanTurn} = this.props;
-    const turnClass = humanTurn? 'turnLabel human': 'turnLabel ai';
-    return (
-      <h3 className={turnClass}>{humanTurn? "Your Turn": "AI Turn"}</h3>
-    );
-  }
-}
+export const TurnLabel = props => {
+  const { humanTurn } = props;
+  const turnClass = humanTurn ? "turnLabel human" : "turnLabel ai";
+  return <h3 className={turnClass}>{humanTurn ? "Your Turn" : "AI Turn"}</h3>;
+};
